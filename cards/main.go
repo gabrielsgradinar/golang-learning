2961e53a83db52f29cf4edcf1ed14ccb6e8fbf04
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-04-19 17:01:03 -0300

    working with type conversion and write into a file

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 6)
-
-	hand.print()
-	fmt.Println("--------------------")
-	remainingCards.print()
+	cards.saveToFile("cards.txt")
 
 }
