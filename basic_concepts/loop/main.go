9806a2a5038164213064f924b864957678712590
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-05-13 17:54:48 -0300

    loops

---
+++ basic_concepts/loop/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// for i := 0; i < 10; {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// j := 10
+	// for j >= 0 { // while loop
+	// 	fmt.Println("j:", j)
+	// 	j--
+	// }
+
+	sum := 0
+	for {
+		sum++
+	}
+}
