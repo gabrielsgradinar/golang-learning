0af6e4c158bd1540c0ebb331cb0648f906f1263e
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-05-14 08:09:23 -0300

    break and continue loop

--- basic_concepts/loop/main.go
+++ basic_concepts/loop/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// for i := 0; i < 10; i++ {
 	// 	fmt.Println(i)
@@ -18,8 +16,26 @@ func main() {
 	// 	j--
 	// }
 
-	sum := 0
-	for {
-		sum++
-	}
+	// sum := 0 // while true
+	// for {
+	// 	sum++
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 != 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// count := 0
+	// for i := 0; true; i++ {
+	// 	if i%13 == 0 {
+	// 		fmt.Printf("%d is divisible by 13 \n", i)
+	// 		count++
+	// 	}
+	// 	if count == 10 {
+	// 		break
+	// 	}
+	// }
 }
