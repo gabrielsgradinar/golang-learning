2c7404a735d39e9009b2761ad2ff3f9629e40017
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-05-22 14:18:18 -0300

    write file with os and ioutil packages

--- basic_concepts/files/main.go
+++ basic_concepts/files/main.go
@@ -1,62 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
-	var newFile *os.File
-	var err error
-
-	newFile, err = os.Create("test.txt")
-
-	if err != nil {
-		// fmt.Println(err)
-		// os.Exit(1)
-
-		log.Fatal(err)
-	}
-
-	err = os.Truncate("test.txt", 0)
+	file, err := os.OpenFile(
+		"test.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0644,
+	)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	newFile.Close()
+	defer file.Close()
 
-	file, err := os.OpenFile("test.txt", os.O_CREATE|os.O_APPEND, 0644)
+	byteSlice := []byte("I learn Golang!")
+	bytesWritten, err := file.Write(byteSlice)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	file.Close()
+	log.Printf("Bytes Written? %d\n", bytesWritten)
 
-	var fileInfo os.FileInfo
-
-	fileInfo, err = os.Stat("test.txt")
-	fmt.Println("File Name:", fileInfo.Name())
-	fmt.Println("Size in bytes:", fileInfo.Size())
-	fmt.Println("Last modified:", fileInfo.ModTime())
-	fmt.Println("Is Directory ?", fileInfo.IsDir())
-	fmt.Println("Permissions:", fileInfo.Mode())
-
-	// fileInfo, err = os.Stat("eita.txt")
-	// if err != nil {
-	// 	if os.IsNotExist(err) {
-	// 		log.Fatal("File does not exist!")
-	// 	}
-	// }
-
-	oldPath := "test.txt"
-	newPath := "ue.txt"
-	err = os.Rename(oldPath, newPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = os.Remove(newPath)
+	bs := []byte("Go Programming is top!")
+	err = ioutil.WriteFile("opa.txt", bs, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
