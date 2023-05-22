6bc2152d1d5fc5fa58568c2a8a292c5af0544059
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-05-22 14:53:22 -0300

    read file line by line with scanner

--- basic_concepts/files/main.go
+++ basic_concepts/files/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 )
@@ -16,32 +15,27 @@ func main() {
 
 	defer file.Close()
 
-	byteSlice := make([]byte, 2)
+	scanner := bufio.NewScanner(file)
 
-	numberBytesRead, err := io.ReadFull(file, byteSlice)
-	if err != nil {
+	scanner.Split(bufio.ScanWords)
+
+	success := scanner.Scan()
+	if success == false {
+		err = scanner.Err()
+		if err == nil {
+			log.Println("Scan was completed and it reached EOF")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("First line found:", scanner.Text())
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Number of bytes read: %d\n", numberBytesRead)
-	fmt.Printf("Data read: %s\n", byteSlice)
-
-	file, err = os.Open("test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Data as string: %s\n", data)
-	fmt.Printf("Number of bytes read: %d\n", len(data))
-
-	data, err = ioutil.ReadFile("test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Data read: %s\n", data)
 
 }
