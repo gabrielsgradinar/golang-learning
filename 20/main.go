429713970f6ad6b725ca0ba8dfa0bb8218c1dd7d
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-11-22 19:33:31 -0300

    for, condicionais e buildando

---
+++ 20/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	a := 1
+	// b := 2
+	// c := 3
+
+	// if a > b || c > a {
+	// 	println("a > b || c > a")
+	// } else {
+	// 	println(b)
+	// }
+
+	switch a {
+	case 1:
+		println("a")
+	default:
+		println("b")
+	}
+}
