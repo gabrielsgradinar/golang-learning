5aa33637802553340ff5470fc8e610279393df4a
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-12-26 17:45:54 -0300

    go mod tidy -e

--- packaging/4/sistema/go.mod
+++ packaging/4/sistema/go.mod
@@ -1,3 +1,5 @@
 module github.com/gabrielsgradinar/golang-learning/packaging/3/sistema
 
 go 1.21.5
+
+require github.com/google/uuid v1.5.0
--- packaging/4/sistema/main.go
+++ packaging/4/sistema/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/gabrielsgradinar/golang-learning/packaging/3/math"
+	"github.com/google/uuid"
 )
 
 func main(){
 	m := math.NewMath(2, 3)
 	fmt.Println(m.Add())
+	fmt.Println(uuid.New().String())
 }
