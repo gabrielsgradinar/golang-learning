97413de36b43ca633e4b51e77904ac7f26f1a71d
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-12-27 12:11:18 -0300

    tests

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
  - go mod tidy
  - go work init ./repative_paths
  - go mod tidy -e
+
+# testes
+
+- go test
+- go test -coverprofile=coverage.out
+- go tool cover -html=coverage.out
+- go test -bench=.
+- go test -bench=. -count=5 -benchtime=3s
+
+- fuzzing - testes de mutação
+- go test -fuzz=. -run=^#
+- go test -fuzz=. -fuzztime=10s -run=^#
---
+++ testes/1/tax_test.go
@@ -0,0 +1,67 @@
+package tax
+
+import "testing"
+
+func TestCalculateTax(t *testing.T){
+	amount := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amount)
+
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+	
+}
+
+func TestCalculateTaxBatch(t *testing.T){
+	type calcTax struct {
+		amount, expect float64
+	}
+
+	assertTable := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+		{0.0, 0.0},
+	}
+
+	for _, item := range assertTable {
+		result := CalculateTax(item.amount)
+		if result != item.expect {
+			t.Errorf("Expected %f but got %f", item.expect, result)
+		}
+	}
+}
+
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500.0)
+	}
+	
+}
+
+func BenchmarkCalculateTax2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
+	}
+	
+}
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{500.0, 1000.0, 1501.0, -1, -2, -2.5}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+
+	f.Fuzz(func(t *testing.T, amount float64){
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0{
+			t.Errorf("Received %f but expected 0", result)
+		}
+
+		if amount > 20000 && result != 20 {
+			t.Errorf("Received %f but expected 20", result)
+		}
+	})
+}
