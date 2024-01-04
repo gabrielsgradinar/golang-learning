4586dcd09aece0de27bca3e81f7f87f8589ce686
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2024-01-04 17:40:03 -0300

    refact router

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -29,11 +29,13 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	r.Route("/products", func(r chi.Router) {
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
 
 	http.ListenAndServe(":8000", r)
 }
